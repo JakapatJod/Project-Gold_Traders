b616a890f63078c727b6b422f92138219b4c7303
JakapatJod <jakapat2332@gmail.com>
2022-10-28 15:38:47 +0700

    dd

--- Flask_Login.py
+++ Flask_Login.py
@@ -1,20 +1,22 @@
 from flask import Flask , render_template
 from flask_sqlalchemy import SQLAlchemy
-# from Flask_Login import UserMixin
+from sqlalchemy import Column,Integer,String,Date # ประเภทของ columns มีอะไรบ้าง
+from flask_login import UserMixin
 # from flask_wtf import wtforms # pip install Flask-WTF
 # from wtforms import StringField , PasswordField , SubmitField
 # from wtforms.validators import InputRequired ,Length ,ValidationError
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://webadmin:RTTooa27373@10.104.4.188:5432/login' # define ของ databaseSQL ดึง database
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://webadmin:RTTooa27373@node37431-project.proen.app.ruk-com.cloud:5432/login' # define ของ databaseSQL ดึง database
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # ปิดข้อความโชว์ ถ้าจะเปิดให้เป็น True
 app.config['SQLALCHEMY_KEY'] = 'secretkey' 
 
-# class User(db.Model,UserMixin):
-#     id = db.Column(db.Integer,primary_key=True)
-#     username = db.Column(db.String(20),nullable=False,unique=True)
-#     password = db.Column(db.String(80), nullable=False)
-
+class User(db.Model,UserMixin):
+    __tablename__ = 'comments' # เรียกใช้ table ที่ชื่อว่า comments
+    id = Column(Integer,primary_key=True) # primary_key คือซ้ำไม่ได้
+    Username = Column(String,nullable=False,unique=True)
+    password = Column(String,nullable=False)
 # class RegisterForm(FlaskFrom):
 #     username = StringField(validators=[InputRequired() , Length(
 #         min=4 , max=20)], render_kw={"placeholder":"Username"})
@@ -22,7 +24,7 @@ app.config['SQLALCHEMY_KEY'] = 'secretkey'
 #     password = StringField(validators=[InputRequired() , Length(
 #         min=4 , max=20)], render_kw={"placeholder":"Password"})
     
-#     submit = SubmitField("Register")
+    # submit = SubmitField("Register")
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -33,10 +35,12 @@ def main():
 
 @app.route('/main/login')
 def login():
+
     return render_template('login.html')
 
 @app.route('/main/register')
 def register():
+
     return render_template('register.html')
 
 if __name__ == '__main__':
